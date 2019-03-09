eb9959494b3ec3990079f5e61489a6157ca332b2
brien84 <marius@mariuss-macbook-pro.local>
2019-03-09 23:40:29 +0200

    Initial Commit

---
+++ Naujienos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Naujienos
+//
+//  Created by Marius on 09/03/2019.
+//  Copyright © 2019 Marius. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
