c9c63421208caeb39d5f7fc72e079ad99526526f
brien84 <marius@mariuss-macbook-pro.local>
2019-03-11 21:30:45 +0200

    Add createDefaultSettings function to AppDelegate.

--- Naujienos/AppDelegate.swift
+++ Naujienos/AppDelegate.swift
@@ -13,34 +13,33 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        createDefaultSettings()
+        
         return true
     }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    
+    /// Check if Settings file already exists and if not create Settings.plist from bundle file DefaultSettings.plist.
+    private func createDefaultSettings() {
+        let fileManager = FileManager.default
+        
+        guard let directory = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first else {
+            fatalError("Could not find documentDirectory.")
+        }
+        
+        let settingsFilePath = directory.appendingPathComponent("Settings.plist").path
+        
+        if !fileManager.fileExists(atPath: settingsFilePath) {
+            guard let defaultsPath = Bundle.main.path(forResource: "DefaultSettings", ofType: "plist") else {
+                fatalError("Could not find DefaultSettings.plist in bundle.")
+            }
+            do {
+                let data = try Data(contentsOf: URL(fileURLWithPath: defaultsPath))
+                fileManager.createFile(atPath: settingsFilePath, contents: data)
+            } catch {
+                fatalError("Could not create default settings.")
+            }
+        }
     }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
 }
-
