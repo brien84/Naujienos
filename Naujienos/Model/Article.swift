02591ebd6840f78625100f70163a539f1490a38f
brien84 <marius@mariuss-macbook-pro.local>
2019-03-12 16:52:41 +0200

    Add Article model.

---
+++ Naujienos/Model/Article.swift
@@ -0,0 +1,21 @@
+//
+//  Article.swift
+//  Naujienos
+//
+//  Created by Marius on 12/03/2019.
+//  Copyright © 2019 Marius. All rights reserved.
+//
+
+import Foundation
+
+struct Article {
+    
+    let url: URL
+    let title: String
+    let date: Date
+    let description: String
+    let imageURL: URL
+    let provider: String
+    let category: String
+    
+}
