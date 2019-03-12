9afff694387703a0343f43d30bebf66a819073c2
brien84 <marius@mariuss-macbook-pro.local>
2019-03-12 22:56:54 +0200

    Add BookmarkButton

---
+++ Naujienos/View/BookmarkButton.swift
@@ -0,0 +1,40 @@
+//
+//  BookmarksButton.swift
+//  Naujienos
+//
+//  Created by Marius on 12/03/2019.
+//  Copyright © 2019 Marius. All rights reserved.
+//
+
+import UIKit
+
+protocol BookmarkButtonProtocol: AnyObject {
+    func bookmarkButtonTapped(_ sender: BookmarkButton, _ gestureRecognizer: UITapGestureRecognizer)
+}
+
+class BookmarkButton: UIButton {
+
+    weak var delegate: BookmarkButtonProtocol?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupButton()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupButton()
+    }
+    
+    private func setupButton() {
+        self.setTitle("X", for: .selected)
+        self.setTitle("O", for: .normal)
+        
+        addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(buttonTapped)))
+    }
+    
+    @objc private func buttonTapped(_ gestureRecognizer: UITapGestureRecognizer) {
+        delegate?.bookmarkButtonTapped(self, gestureRecognizer)
+    }
+
+}
